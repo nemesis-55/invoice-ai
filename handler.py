b18980ba963a461ebf9436ae59fa4cb3bf3f7f26
Saurav <saurav.kumar3@phonepe.com>
2024-12-09 05:21:39 +0530

    loading adaptor directly

--- handler.py
+++ handler.py
@@ -28,9 +28,9 @@ handler = PrintAndLogHandler()
 logger.addHandler(handler)
 
 # Install flash-attn
-# logger.info("Installing flash-attn...")
-# subprocess.check_call([os.sys.executable, "-m", "pip", "install", "flash-attn"])
-# logger.info("flash-attn installed successfully.")
+logger.info("Installing flash-attn...")
+subprocess.check_call([os.sys.executable, "-m", "pip", "install", "flash-attn"])
+logger.info("flash-attn installed successfully.")
 
 
 # Constants
