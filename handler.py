43e25e749e0daad8ca96355bf30ec17e1989008b
nemesis-55 <sauravkumarsingh981@gmail.com>
2024-12-11 19:55:57 +0530

    Update handler.py
    runpod-volume cache

--- handler.py
+++ handler.py
@@ -15,7 +15,7 @@ import numpy
 MODEL_DPI = 600
 MODEL_TYPE = "openbmb/MiniCPM-V-2_6"
 ADAPTOR_TYPE = "Zorro123444/invoice_extracter_2"
-adaptor_dir = "./cache/adaptor"
+adaptor_dir = "./runpod-volume/cache/adaptor"
 print("login to hugging face")
 login("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")
 
