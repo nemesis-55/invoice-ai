a930ea742674a5b5989c9653b985172aa504167f
Saurav Kumar <saurav.kumar3@phonepe.com>
2025-04-23 21:59:14 +0530

    300 dpi, rgba scale, max_token = 2048

--- handler.py
+++ handler.py
@@ -131,7 +131,7 @@ def perform_inference(messages, model, tokenizer):
     """Perform model inference."""
     try:
         with torch.no_grad():
-            response = model.chat(image=None, msgs=messages, tokenizer=tokenizer, max_new_tokens=1024)
+            response = model.chat(image=None, msgs=messages, tokenizer=tokenizer, max_new_tokens=2048)
         return response
     except Exception as e:
         print(f"Inference failed: {e}")
