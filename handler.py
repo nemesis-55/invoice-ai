46cef4710ab16a58eb74717c8324efdb04cb433e
Aditi Chandorkar <aditi.chandorkar@gothiadigital.com>
2025-08-11 12:00:48 +0530

    (fix) : updated adaptor type and requirements for consistency

--- handler.py
+++ handler.py
@@ -18,7 +18,7 @@ import json
 
 # Constants
 MODEL_DPI = 300
-ADAPTOR_TYPE = "GothiaDigitalSolutions/invoice-extractor-3.0"
+ADAPTOR_TYPE = "GothiaDigitalSolutions/invoice-extractor-2.0"
 cache = "/runpod-volume/cache"
 login(os.getenv("HF_TOKEN"))
 
