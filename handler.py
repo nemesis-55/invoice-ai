94f811f9e92e129212604c329edb2e414fe82d94
Arindam Upadhyay <arindamupadhyay2002@gmail.com>
2025-09-16 10:21:55 +0530

    refactor: remove COMMIT_HASH from model loading and update requirements for deepspeed versioning

--- handler.py
+++ handler.py
@@ -24,7 +24,6 @@ os.makedirs(CACHE_DIR, exist_ok=True)
 # Constants
 MODEL_DPI = 300
 ADAPTOR_TYPE = "GothiaDigitalSolutions/invoice-extractor-3.0"
-COMMIT_HASH = "8c16acb8e756a7e72f93da7745156d50f2f50fc8"
 cache = os.environ["HF_HOME"]
 
 # One-time cache cleanup (remove old unreferenced revisions to free space)
@@ -60,10 +59,7 @@ def load_model_and_tokenizer():
     """Load the main model and tokenizer."""
     try:
         print("Loading tokenizer")
-        tokenizer = AutoTokenizer.from_pretrained(
-                                ADAPTOR_TYPE,
-                                trust_remote_code=True,
-                                revision=COMMIT_HASH)
+        tokenizer = AutoTokenizer.from_pretrained(ADAPTOR_TYPE, trust_remote_code=True)
         print("Loading model...")
         model = AutoModel.from_pretrained(
             ADAPTOR_TYPE,
@@ -71,8 +67,7 @@ def load_model_and_tokenizer():
             attn_implementation="sdpa",
             trust_remote_code=True, 
             torch_dtype=torch.bfloat16, 
-            cache_dir=cache,
-            revision=COMMIT_HASH
+            cache_dir=cache
         ).cuda().eval()
         messages = [
             {"role": "user", "content": "hey"}
